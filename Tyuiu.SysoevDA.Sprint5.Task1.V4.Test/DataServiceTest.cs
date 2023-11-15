347fbadacb0bcca5632564386aa94dd41cc02a8d
SysoevDA <fuzzii2739@gmail.com>
2023-11-15 10:52:34 +0500

    Task1

---
+++ Tyuiu.SysoevDA.Sprint5.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.SysoevDA.Sprint5.Task1.V4.Lib;
+
+namespace Tyuiu.SysoevDA.Sprint5.Task1.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\Admin\source\repos\C#\Tyuiu.SysoevDA.Sprint5\Tyuiu.SysoevDA.Sprint5.Task1.V4\bin\Debug\OutPutFileTask1.txt";
+            
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExist = fileinfo.Exists;
+            
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.SysoevDA.Sprint5.Task1.V4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.SysoevDA.Sprint5.Task1.V4.Lib;
+
+namespace Tyuiu.SysoevDA.Sprint5.Task1.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Сысоев Д.А | ПКТБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Запись набора данных в текстовый файл                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Сысоев Данил Алексеевич | ПКТБ-23-1                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция, F(x) = cos(x)/x+1  - cos(x) * 1,3 + 3х f(x) на заданном   *");
+            Console.WriteLine("* диапазоне [-5;5] с шагом 1. Произвести проверку деления на ноль. При    *");
+            Console.WriteLine("* делении на ноль вернуть значение 0. Результат сохранить в текстовый     *");
+            Console.WriteLine("* файл OutPutFileTask1.txt и вывести на консоль. Округлить до двух знаков *");
+            Console.WriteLine("* после запятой.                                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue,stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            string[] lines = File.ReadAllLines(res);
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    f(x)  |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= lines.Length - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, lines[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
